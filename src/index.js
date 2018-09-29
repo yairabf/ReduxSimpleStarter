c6fff58c3b64bfca0356de6ddef85689790e3520
yair.abramovitch <yair.abramovitch@servicenow.com>
2018-09-30 02:04:04 +0300

    1. Changing the port to 3000 2. adding list of videos. 3. creating a video player. 4. adding the select video functionality

--- src/index.js
+++ src/index.js
@@ -2,19 +2,39 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyAdt-DVsq6C_q2crb9pz4Mw58LLncnJo_c';
 
 // Create a new component. This component should produce some HTML
-YTSearch({key: API_KEY, term: 'MKBHD'}, function(data) {
-  console.log(data);
-});
+
 
 class App extends Component {
-    render () {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    YTSearch({key: API_KEY, term: 'MKBHD'}, (videos) => {
+      this.setState({ 
+        videos: videos,
+        selectedVideo:videos[0]
+      }); 
+    });
+  }
+
+  render () {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+            videos={this.state.videos} />
       </div>
     );
   }
