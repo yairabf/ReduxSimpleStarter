1a2dc3cb98fd49586a3d18cb3a9f7249f86bd076
yair.abramovitch <yair.abramovitch@servicenow.com>
2018-09-30 02:24:44 +0300

    adding search capability

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
-
     constructor(props) {
         super(props);
 
@@ -10,12 +9,17 @@ class SearchBar extends Component {
 
     render(){
         return (
-            <div>
+            <div className="search-bar">
                 <input 
                 value = {this.state.term}
-                onChange={event => this.setState({ term: event.target.value })}/>
+                onChange={event => this.onInputChange(event.target.value)}/>
             </div>
         );
+    }
+
+    onInputChange(term) {
+        this.setState({term});
+        this.props.onSearchTermChange(term);
     }
 
 }
